3ff288e2aec095caa93489beba5243efee85b240
Dostonbek Toirov <toirovd@berea.edu>
2019-11-16 14:26:06 -0500

    Working app

--- app/src/main/java/com/example/instaparse/LoginActivity.java
+++ app/src/main/java/com/example/instaparse/LoginActivity.java
@@ -2,13 +2,22 @@ package com.example.instaparse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 public class LoginActivity extends AppCompatActivity {
 
+    public static final String TAG = "LoginActivity";
+    
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
@@ -33,5 +42,25 @@ public class LoginActivity extends AppCompatActivity {
 
     private void login(String username, String password) {
         // Login
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with login");
+                    e.printStackTrace();
+                    return;
+                }
+                goMainActivity();
+            }
+
+        });
+    }
+
+
+    private void goMainActivity() {
+        Log.e(TAG, "Navigate to main activity");
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
