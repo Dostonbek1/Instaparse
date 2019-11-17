b38a3dc791c9778e39708a4152a6f0cbc280a00c
Dostonbek Toirov <toirovd@berea.edu>
2019-11-17 12:12:25 -0500

    Sign up funtionality added

--- app/src/main/java/com/example/instaparse/LoginActivity.java
+++ app/src/main/java/com/example/instaparse/LoginActivity.java
@@ -21,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +31,7 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,6 +40,15 @@ public class LoginActivity extends AppCompatActivity {
                 login(username, password);
             }
         });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e(TAG, "Navigate to Sign Up activity");
+                Intent i = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 
     private void login(String username, String password) {
